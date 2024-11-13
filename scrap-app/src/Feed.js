050c39a88ab6d91423a08f629590c7a0be29c99a
night-pianist <sweetkhiet907@gmail.com>
2024-11-13 00:44:22 -0800

    set up feed

--- scrap-app/src/Feed.js
+++ scrap-app/src/Feed.js
@@ -3,6 +3,9 @@ import threadImage from './assets/thread.png';
 export default function Feed() {
     <div className="feed">
         <img src={threadImage} />
+        <div>
+            test
+        </div>
   
     </div>
 }
