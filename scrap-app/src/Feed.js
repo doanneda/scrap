dcbe97a15387d61b93438e5cb0b50ca3fc35e393
Anne Do <doanneda@gmail.com>
2024-11-19 01:11:27 -0800

    linked feed and login with routing

--- scrap-app/src/Feed.js
+++ scrap-app/src/Feed.js
@@ -1,11 +1,17 @@
 import threadImage from './assets/thread.png';
+import { Link } from 'react-router-dom';
 
 export default function Feed() {
-    <div className="feed">
+    return (
+    <div className="feed" alt="brown thread">
         <img src={threadImage} />
         <div>
             test
         </div>
-  
+
+        <Link to="login">
+            <button>Login</button>
+        </Link>
     </div>
+    )
 }
