b73af027ad260ad55dbf5059345ee8dc252bd49f
Jasmine <jjasminew16@gmail.com>
2024-11-24 16:17:54 -0800

    added CreatePage component that includes adding images and text

--- scrap-app/src/App.js
+++ scrap-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login.js';
+import CreatePage from './CreatePage.js';
 
 import Feed from './Feed.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -11,7 +12,7 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Feed />} />
+            <Route path="/" element={<CreatePage />} />
             <Route path="/login" element={<Login />} />
           </Routes>
       </BrowserRouter>
