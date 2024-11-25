526c7c1d51ceb19aa5f2893fdd0b6608fbf05de1
night-pianist <sweetkhiet907@gmail.com>
2024-11-24 17:15:38 -0800

    finish backend server for scrappages

--- server/models/scrapPageModel.js
+++ server/models/scrapPageModel.js
@@ -5,43 +5,43 @@ const scrapPageSchema = new mongoose.Schema({
     required: true,
     type: String,
   },
-  // images: { // images for scrap book page 
-  //   required: false,
-  //   type: [String], // Array of strings to store image URLs
-  //     // ex. "images": ["https://example.com/image1.jpg", "https://example.com/image2.jpg"]
-  // },
-  // text: { // text for scrap book page
-  //   required: false,
-  //   type: String
-  // },
-  // color: {
-  //   required: false, // default can be white
-  //   type: String, // pastel colors 
-  // },
-  // stickers: { // store number, position, and type of stickers
-  //   required: false, 
-  //   type: [
-  //     {
-  //       stickerType: { 
-  //         type: String, required: true 
-  //       }, // sticker type (ex. 'emoji', 'star', etc.)
-  //       position: {
-  //         type: [
-  //           {
-  //             x: { // x position in percentage
-  //               type: Number, 
-  //               required: true 
-  //             }, 
-  //             y: { // y position in percentage
-  //               type: Number, 
-  //               required: true 
-  //             }, 
-  //           },
-  //         ],
-  //       },
-  //     },
-  //   ],
-  // }
+  images: { // images for scrap book page 
+    required: false,
+    type: [String], // Array of strings to store image URLs
+      // ex. "images": ["https://example.com/image1.jpg", "https://example.com/image2.jpg"]
+  },
+  text: { // text for scrap book page
+    required: false,
+    type: String
+  },
+  color: {
+    required: false, // default can be white
+    type: String, // pastel colors 
+  },
+  stickers: { // store number, position, and type of stickers
+    required: false, 
+    type: [
+      {
+        stickerType: { 
+          type: String, required: true 
+        }, // sticker type (ex. 'emoji', 'star', etc.)
+        position: {
+          type: [
+            {
+              x: { // x position in percentage
+                type: Number, 
+                required: true 
+              }, 
+              y: { // y position in percentage
+                type: Number, 
+                required: true 
+              }, 
+            },
+          ],
+        },
+      },
+    ],
+  }
 });
 
 module.exports = mongoose.model('ScrapPage', scrapPageSchema);
--- server/routes/scrapPageRoute.js
+++ server/routes/scrapPageRoute.js
@@ -6,24 +6,4 @@ const scrapPageController = require('../controllers/scrapPageController'); // ge
 
 scrapPageRouter.post('/post', scrapPageController.createScrapPage);
 
-// scrapPageRouter.get('/', async (req, res) => {
-//     try {
-//       const scrapPages = await scrapPage.find(); // fetch all scrap pages from the database
-//       res.status(200).json(scrapPages); // return all scrap pages as JSON
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ error: 'Error fetching scrap pages' });
-//     }
-//   });
-
-// scrapPageRouter.get('/get', async (req, res) => {
-//   try {
-//     res.send(randomPrompt);
-//   } catch (err) {
-//     console.error(err);
-//     // Handle the error appropriately
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
 module.exports = scrapPageRouter;
