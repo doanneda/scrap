db8a86fd0ef2b578730be68e13b4759b03f44be4
lavinialei2 <lavinialei@g.ucla.edu>
2024-11-24 21:51:38 -0800

    Sticker images

--- scrap-app/src/DrapAndDrop.jsx
+++ scrap-app/src/DrapAndDrop.jsx
@@ -10,16 +10,19 @@ export default function DragAndDrop() {
   const [images, setImages] = useState([
     { id: 'image1', x: 0, y: 0,
       imageUrl: 'https://media.discordapp.net/attachments/1297781942590378108/1309054813966958663/IMG_2665.jpg?ex=6744cd0c&is=67437b8c&hm=f92f9225d7f41588f2e58f1e3dac31621bcca79b7800447a12d54bb127cd8831&=&format=webp&width=1248&height=936',
-      size: {width: 240, height: 160} },
+      size: {width: 220, height: 160} },
     { id: 'image2', x: 0, y: 200,
       imageUrl: 'https://media.discordapp.net/attachments/1297781942590378108/1301389329817403493/IMG_4203.jpg?ex=67454242&is=6743f0c2&hm=04ca92b4bf16431b923b3e631f9175f626e502b66482124d94347fc16c6211cf&=&format=webp&width=1248&height=936',
-      size: {width: 240, height: 160}},
+      size: {width: 220, height: 160}},
     { id: 'image3', x: -300, y: 0,
       imageUrl: 'https://media.discordapp.net/attachments/1297781942590378108/1300694734745894962/IMG_4197.jpg?ex=6744b59e&is=6743641e&hm=ec630912d3923374486fe760103bce798a3c367c74dfed3056fd58370713fdd2&=&format=webp&width=1248&height=936',
-      size: {width: 240, height: 160}},
+      size: {width: 220, height: 160}},
     { id: 'image4', x: -300, y: 200,
       imageUrl: 'https://media.discordapp.net/attachments/1297781942590378108/1301389323136139316/IMG_4216.jpg?ex=67454241&is=6743f0c1&hm=5f28de06a5df46166c363257a430e124e8a968b0524de275acc46cde515a37d1&=&format=webp&width=1248&height=936',
-      size: {width: 240, height: 160}},
+      size: {width: 220, height: 160}},
+    { id: 'image5', x: -300, y: 400,
+      imageUrl: 'https://media.discordapp.net/attachments/1297781942590378108/1300694735567982664/IMG_4194.jpg?ex=6744b59e&is=6743641e&hm=913b88a0d33155c7da1852b1e1b8e95f37d07c1f39cdfe23ca7ca6ed88f07cfc&=&format=webp&width=702&height=936',
+      size: {width: 160, height: 200}},
   ]);
 
   const [bounds, setBounds] = useState({ top: 0, left: 0, bottom: 0, right: 0 });
