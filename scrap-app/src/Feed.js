85ecec3841145a6b8ee357d7d47c821e165bb180
Jasmine <jjasminew16@gmail.com>
2024-11-25 01:49:35 -0800

    fixing server

--- scrap-app/src/Feed.js
+++ scrap-app/src/Feed.js
@@ -50,18 +50,20 @@ export default function Feed() {
             {page.binaryImages && page.binaryImages.length > 0 ? (
               <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))', gap: '10px' }}>
                 {page.binaryImages.map((image, index) => {
-                    const bufferToBase64 = (bufferObj) => {
-                        const byteArray = new Uint8Array(bufferObj.data); // Convert to Uint8Array
-                        const base64String = new TextDecoder().decode(byteArray); // Decode to Base64 string
-                        return base64String; // The result should include 'data:image/...'
-                      };
-                      
-                    const imageSrc = bufferToBase64(image);
+
+                    // const bufferToBase64 = (bufferObj) => {
+                    //     const byteArray = new Uint8Array(bufferObj.data); // Convert to Uint8Array
+                    //     const base64String = new TextDecoder().decode(byteArray); // Decode to Base64 string
+                    //     return base64String; // The result should include 'data:image/...'
+                    // };
+                    
+                    console.log(image)
+                    // const imageSrc = bufferToBase64(image);
                       
                     return (
                         <img
                         key={index}
-                        src={imageSrc}
+                        src={image}
                         alt={`Scrapbook ${index}`}
                         style={{ width: '150px', height: '150px', objectFit: 'cover' }}
                         />
@@ -95,17 +97,4 @@ export default function Feed() {
       </div>
     </div>
   );
-
-    // return (
-    // <div className="feed" alt="brown thread">
-    //     <img src={threadImage} />
-    //     <div>
-    //         test
-    //     </div>
-
-    //     <Link to="login">
-    //         <button>Login</button>
-    //     </Link>
-    // </div>
-    // )
 }
