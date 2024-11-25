3dd48b8ab26dcaba6e56f7a32c794e3817bea35c
Jasmine <jjasminew16@gmail.com>
2024-11-24 17:08:53 -0800

    tested with insomnia and tested with mongodb

--- server/controllers/scrapPageController.js
+++ server/controllers/scrapPageController.js
@@ -4,8 +4,8 @@ const createScrapPage = async (req, res) => {
   const newScrapPage = new ScrapPage(req.body); // create a new instance of ScrapPage 
   try {
     const savedScrapPage = await newScrapPage.save(newScrapPage);  // save the scrap page to Mongo
-    res.send(data);
-    res.status(201).json(savedScrapPage);
+    res.send(savedScrapPage);
+    // res.status(201).json(savedScrapPage);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Error saving the scrap page' });
--- server/models/scrapPageModel.js
+++ server/models/scrapPageModel.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose');
 
-const scrapPage = new mongoose.Schema({
+const scrapPageSchema = new mongoose.Schema({
   name: { // user name or id associated with the scrap book page
     required: true,
     type: String,
   },
-  images: { // images for scrap book page 
-    required: false,
-    type: [String], // Array of strings to store image URLs
-      // ex. "images": ["https://example.com/image1.jpg", "https://example.com/image2.jpg"]
-  },
-  text: { // text for scrap book page
-    required: false,
-    type: String
-  },
-  color: {
-    required: false, // default can be white
-    type: String, // pastel colors 
-  },
-  stickers: { // store number, position, and type of stickers
-    required: false, 
-    type: [
-      {
-        stickerType: { 
-          type: String, required: true 
-        }, // sticker type (ex. 'emoji', 'star', etc.)
-        position: {
-          type: [
-            {
-              x: { // x position in percentage
-                type: Number, 
-                required: true 
-              }, 
-              y: { // y position in percentage
-                type: Number, 
-                required: true 
-              }, 
-            },
-          ],
-        },
-      },
-    ],
-  }
+  // images: { // images for scrap book page 
+  //   required: false,
+  //   type: [String], // Array of strings to store image URLs
+  //     // ex. "images": ["https://example.com/image1.jpg", "https://example.com/image2.jpg"]
+  // },
+  // text: { // text for scrap book page
+  //   required: false,
+  //   type: String
+  // },
+  // color: {
+  //   required: false, // default can be white
+  //   type: String, // pastel colors 
+  // },
+  // stickers: { // store number, position, and type of stickers
+  //   required: false, 
+  //   type: [
+  //     {
+  //       stickerType: { 
+  //         type: String, required: true 
+  //       }, // sticker type (ex. 'emoji', 'star', etc.)
+  //       position: {
+  //         type: [
+  //           {
+  //             x: { // x position in percentage
+  //               type: Number, 
+  //               required: true 
+  //             }, 
+  //             y: { // y position in percentage
+  //               type: Number, 
+  //               required: true 
+  //             }, 
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   ],
+  // }
 });
 
-module.exports = mongoose.model('ScrapPage', scrapPage);
+module.exports = mongoose.model('ScrapPage', scrapPageSchema);
